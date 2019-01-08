8535c0c94d4c2b3d8924828123e583acddfb27a4
LaySent <laysent@gmail.com>
2019-01-08 20:46:40 +0800

    Updates

--- precache-manifest.288ee8a96337a58b8ad120c6ab598275.js
+++ precache-manifest.288ee8a96337a58b8ad120c6ab598275.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/sri-hash-generator/static/js/runtime~main.e26c4093.js"
   },
   {
-    "revision": "85c180c52f57c1a9fb1c",
-    "url": "/sri-hash-generator/static/js/main.85c180c5.chunk.js"
+    "revision": "452f4061f8751767f9aa",
+    "url": "/sri-hash-generator/static/js/main.452f4061.chunk.js"
   },
   {
     "revision": "7ed96bb6eaaafaecf181",
     "url": "/sri-hash-generator/static/js/1.7ed96bb6.chunk.js"
   },
   {
-    "revision": "85c180c52f57c1a9fb1c",
+    "revision": "452f4061f8751767f9aa",
     "url": "/sri-hash-generator/static/css/main.63bc41f3.chunk.css"
   },
   {
-    "revision": "18b71d097dd34c672f9238bbd5e3cf45",
+    "revision": "26b62dd7bb43520c6c48d662418d8c3b",
     "url": "/sri-hash-generator/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/sri-hash-generator/precache-manifest.cfc15d9424d9a4188e76d274dda6932b.js"
+  "/sri-hash-generator/precache-manifest.288ee8a96337a58b8ad120c6ab598275.js"
 );
 
 workbox.clientsClaim();
